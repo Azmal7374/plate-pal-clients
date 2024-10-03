0dd3b67193355612cfaacb7e3b700f2ad2a5b5b1
Azmal <azmalgazi8@gmail.com>
2024-10-03 17:53:21 +0600

    create envCongif

--- src/lib/AxiosInstance/index.ts
+++ src/lib/AxiosInstance/index.ts
@@ -1,49 +1,48 @@
 /* eslint-disable padding-line-between-statements */
 /* eslint-disable prettier/prettier */
-
+/* eslint-disable import/order */
+/* eslint-disable prettier/prettier */
+import envConfig from "@/src/config/envConfig";
 import axios from "axios";
 import { cookies } from "next/headers";
 
-
-
 const axiosInstance = axios.create({
-    baseURL: envConfig.baseApi,
-})
-
+  baseURL: envConfig.baseApi,
+});
 
 axiosInstance.interceptors.request.use(
-    function (config){
-        const cookieStore = cookies();
-        const accessToken = cookieStore.get("accessToken")?.value;
+  function (config) {
+    const cookieStore = cookies();
+    const accessToken = cookieStore.get("accessToken")?.value;
 
-        if(accessToken){
-            config.headers.Authorization = `Bearer ${accessToken}`
-        }
-        return config;
+    if (accessToken) {
+      config.headers.Authorization = `Bearer ${accessToken}`;
     }
-    function (error){
-        return Promise.reject(error)
-    }
-)
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
 
 axiosInstance.interceptors.response.use(
-  function (response){
-    return response
-  }
-  async function(error){
-    const config =error.config;
+  function (response) {
+    return response;
+  },
+  async function (error) {
+    const config = error.config;
 
-    if(error?.response.status === 401 && !config?.sent){
-        config.sent= true;
-        const res=await getNewAccessToken();
-        const accessToken = res.data.accessToken;
+    if (error?.response.status === 401 && !config?.sent) {
+      config.sent = true;
+      const res = await getNewAccessToken();
+      const accessToken = res.data.accessToken;
 
-        config.headers["Authirization"] = `Bearer ${accessToken}`;
-        cookies().set("accessToken", accessToken);
+      config.headers["Authirization"] = `Bearer ${accessToken}`;
+      cookies().set("accessToken", accessToken);
 
-        return axiosInstance(config);
+      return axiosInstance(config);
     } else {
-        return Promise.reject(error)
+      return Promise.reject(error);
     }
-  }
-)
+  },
+);
